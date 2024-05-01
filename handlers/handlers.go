0864c615e5a475e1bb7a48f5bae17c1b1f37d460
ky0yk <40429527+ky0yk@users.noreply.github.com>
2024-05-02 07:50:29 +0900

    2.5

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -17,6 +18,23 @@ func HelloHandler (w http.ResponseWriter, req *http.Request){
 
 func PostArticleHandler (w http.ResponseWriter, req *http.Request){
 	article := models.Article1
+
+	// 1. バイトスライス reqBodybuffer を何らかの形で用意
+	length, err := strconv.Atoi(req.Header.Get("Content-Length"))
+	if err != nil {
+		http.Error(w, "cannot get content lenght\n", http.StatusBadRequest)
+		return
+	}
+	reqBodyBuffer := make([]byte, length)
+
+	// 2. Read メソッドでリクエストボディを読み出し
+	if _, err := req.Body.Read(reqBodyBuffer); !errors.Is(err, io.EOF) {
+		http.Error(w, "fail to get request body\n", http.StatusBadRequest)
+	}
+
+	// 3. ボディを Close する
+	defer req.Body.Close()
+
 	jsonData, err := json.Marshal(article)
 	if err != nil {
 		http.Error(w, "fail to encode json\n", http.StatusInternalServerError)

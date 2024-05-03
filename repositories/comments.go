ba65c1fae59d66c22b8335c36c3e5d966ea0d728
ky0yk <40429527+ky0yk@users.noreply.github.com>
2024-05-04 08:28:49 +0900

    sec 3.8

---
+++ repositories/comments.go
@@ -0,0 +1,54 @@
+package repositories
+
+import (
+	"database/sql"
+
+	"github.com/ky0yk/go-blog/models"
+)
+
+func InsertComment(db *sql.DB, comment models.Comment) (models.Comment, error) {
+	const sqlStr = `
+		insert into comments (article_id, message, created_at) values
+		(?, ?, now());
+	`
+
+	var newComment models.Comment
+	result, err := db.Exec(sqlStr, comment.ArticleID, comment.Message, comment.CreatedAt)
+	if err != nil {
+		return models.Comment{}, err
+	}
+
+	id, _ := result.LastInsertId()
+	newComment.CommentID = int(id)
+
+	return newComment, nil
+	}
+
+func SelectCommentList(db *sql.DB, articleID int) ([]models.Comment, error) {
+	const sqlStr = `
+		select *
+		from comments
+		where article_id = ?;
+	`
+
+	rows, err := db.Query(sqlStr, articleID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	commentArray := make([]models.Comment, 0)
+	for rows.Next(){
+		var comment models.Comment
+		var createdTime sql.NullTime
+		rows.Scan(&comment.ArticleID, &comment.CommentID, &comment.Message, &createdTime)
+
+		if createdTime.Valid {
+			comment.CreatedAt = createdTime.Time
+		}
+
+		commentArray = append(commentArray, comment)
+	}
+
+	return commentArray, nil
+}

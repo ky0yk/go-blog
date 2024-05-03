6a9c63d302268803ba6268b3c0f5d3ba5864fa3e
ky0yk <40429527+ky0yk@users.noreply.github.com>
2024-05-03 20:32:29 +0900

    Sec 3.2

--- main.go
+++ main.go
@@ -1,25 +1,42 @@
 package main
 
 import (
-	"log"
-	"net/http"
+	"database/sql"
+	"fmt"
 
-	"github.com/gorilla/mux"
-	"github.com/ky0yk/go-blog/handlers"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	r := mux.NewRouter()
+	dbUser := "docker"
+	dbPassword := "docker"
+	dbDatabase := "sampledb"
+	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
 
-	r.HandleFunc("/hello", handlers.HelloHandler).Methods((http.MethodGet))
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
 
-	r.HandleFunc("/article", handlers.PostArticleHandler).Methods((http.MethodPost))
-	r.HandleFunc("/article/list", handlers.ArticListleHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/{id:[0-9]+}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
+	if err := db.Ping(); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("connect to DB")
+	}
 
-	r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
+	
+	// r := mux.NewRouter()
 
-	log.Println("server start at port 8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	// r.HandleFunc("/hello", handlers.HelloHandler).Methods((http.MethodGet))
+
+	// r.HandleFunc("/article", handlers.PostArticleHandler).Methods((http.MethodPost))
+	// r.HandleFunc("/article/list", handlers.ArticListleHandler).Methods(http.MethodGet)
+	// r.HandleFunc("/article/{id:[0-9]+}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
+	// r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
+
+	// r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
+
+	// log.Println("server start at port 8080")
+	// log.Fatal(http.ListenAndServe(":8080", r))
 }

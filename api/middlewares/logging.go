aa9f4d536e661f18da4a3e1857ed2ce93baa5f6a
ky0yk <40429527+ky0yk@users.noreply.github.com>
2024-05-12 08:29:45 +0900

    sec 9.3

--- api/middlewares/logging.go
+++ api/middlewares/logging.go
@@ -5,11 +5,29 @@ import (
 	"net/http"
 )
 
+type resLogginWriter struct {
+	http.ResponseWriter
+	code int
+}
+
+func NewResLoggingWriter(w http.ResponseWriter) *resLogginWriter {
+	return &resLogginWriter{ResponseWriter: w, code: http.StatusOK}
+}
+
+func (rsw *resLogginWriter) WriteHeader(code int) {
+	rsw.code = code
+	rsw.ResponseWriter.WriteHeader(code)
+}
+
 func LoggingMiddleWare (next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request){
 
 		log.Println(req.RequestURI, req.Method)
 
-		next.ServeHTTP(w, req)
+		rlw := NewResLoggingWriter(w)
+
+		next.ServeHTTP(rlw, req)
+
+		log.Println("res: ", rlw.code)
 	})
 }

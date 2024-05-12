4f66adb9561e3c477e82a5c87fbc2a022a20d700
ky0yk <40429527+ky0yk@users.noreply.github.com>
2024-05-13 08:32:34 +0900

    sec 10.3

--- services/article_service.go
+++ services/article_service.go
@@ -3,6 +3,7 @@ package services
 import (
 	"database/sql"
 	"errors"
+	"sync"
 
 	"github.com/ky0yk/go-blog/apperrors"
 	"github.com/ky0yk/go-blog/models"
@@ -39,18 +40,43 @@ func (s *MyAppService) GetArticleListService(page int) ([]models.Article, error)
 // ArticleDetailHandlerで使うことを想定したサービス
 // 指定IDの記事情報を返却
 func (s *MyAppService) GetArticleService(articleID int) (models.Article, error) {
-	article, err := repositories.SelectArticleDetail(s.db, articleID)
-	if err != nil {
-		if errors.Is(err, sql.ErrNoRows){
-			err = apperrors.NAData.Wrap(err, "no data")
-			return models.Article{}, err
+	var article models.Article
+	var commentList []models.Comment
+	var articleGetErr, commentGetErr error
+	
+	var amu sync.Mutex
+	var cmu sync.Mutex
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	
+	go func(db *sql.DB, articleID int) {
+		defer wg.Done()
+		amu.Lock()
+		article, articleGetErr = repositories.SelectArticleDetail(db, articleID)
+		amu.Unlock()
+	}(s.db, articleID)
+
+	go func(db *sql.DB, articleID int) {
+		defer wg.Done()
+		cmu.Lock()
+		commentList, commentGetErr = repositories.SelectCommentList(db, articleID)
+		cmu.Unlock()
+	}(s.db, articleID)
+	
+	wg.Wait()
+
+	if articleGetErr != nil {
+		if errors.Is(articleGetErr, sql.ErrNoRows){
+			articleGetErr = apperrors.NAData.Wrap(articleGetErr, "no data")
+			return models.Article{}, articleGetErr
 		}
-		err = apperrors.GetDataFailed.Wrap(err, "fail to get data")
-		return models.Article{}, err
+		articleGetErr = apperrors.GetDataFailed.Wrap(articleGetErr, "fail to get data")
+		return models.Article{}, articleGetErr
 	}
-	commentList, err := repositories.SelectCommentList(s.db, articleID)
-	if err != nil {
-		return models.Article{}, err
+
+	if commentGetErr != nil {
+		return models.Article{}, commentGetErr
 	}
 
 	article.CommentList = append(article.CommentList, commentList...)

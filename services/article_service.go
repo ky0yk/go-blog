ca0d437ff2d48b63f19bcf2d54d1438a5b687f1e
ky0yk <40429527+ky0yk@users.noreply.github.com>
2024-05-13 09:05:39 +0900

    sec 10.4

--- services/article_service.go
+++ services/article_service.go
@@ -3,7 +3,6 @@ package services
 import (
 	"database/sql"
 	"errors"
-	"sync"
 
 	"github.com/ky0yk/go-blog/apperrors"
 	"github.com/ky0yk/go-blog/models"
@@ -43,29 +42,40 @@ func (s *MyAppService) GetArticleService(articleID int) (models.Article, error) 
 	var article models.Article
 	var commentList []models.Comment
 	var articleGetErr, commentGetErr error
-	
-	var amu sync.Mutex
-	var cmu sync.Mutex
 
-	var wg sync.WaitGroup
-	wg.Add(2)
+	type articleResult struct {
+		article models.Article
+		err error
+	}
+	articleChan := make(chan articleResult)
+	defer close(articleChan)
 	
-	go func(db *sql.DB, articleID int) {
-		defer wg.Done()
-		amu.Lock()
-		article, articleGetErr = repositories.SelectArticleDetail(db, articleID)
-		amu.Unlock()
-	}(s.db, articleID)
+	go func(ch chan<- articleResult, db *sql.DB, articleID int) {
+		article, err := repositories.SelectArticleDetail(db, articleID)
+		ch <- articleResult{article: article, err: err}
+	}(articleChan, s.db, articleID)
 
-	go func(db *sql.DB, articleID int) {
-		defer wg.Done()
-		cmu.Lock()
-		commentList, commentGetErr = repositories.SelectCommentList(db, articleID)
-		cmu.Unlock()
-	}(s.db, articleID)
+	type commentResult struct {
+		commentList *[]models.Comment
+		err error
+	}
+	commentChan := make(chan commentResult)
+	defer close(commentChan)
+
+	go func(ch chan<-commentResult, db *sql.DB, articleID int) {
+		commentList, err := repositories.SelectCommentList(db, articleID)
+		ch <- commentResult{commentList: &commentList, err: err}
+	}(commentChan,s.db, articleID)
+
+	for i := 0; i < 2; i++ {
+		select {
+			case ar := <-articleChan:
+				article, articleGetErr = ar.article, ar.err
+			case cr := <-commentChan:
+				commentList, commentGetErr = *cr.commentList, cr.err
+		}
+	}
 	
-	wg.Wait()
-
 	if articleGetErr != nil {
 		if errors.Is(articleGetErr, sql.ErrNoRows){
 			articleGetErr = apperrors.NAData.Wrap(articleGetErr, "no data")
